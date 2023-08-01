d8ea40a2e3c6b6548adb3e1ab996d3b64d4f3662
khingbob <bobur.khayitov2003@gmail.com>
2023-08-01 15:04:26 +0200

    debuggin v2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,8 +8,8 @@ export default () => {
       {allModels.map((model) => (
         <div id={model.name} key={model.name}>
           <picture className={css.imageContainer}>
-            <source srcSet={model.src} media="(min-width: 720px)" />
-            <source srcSet={model.srcV} media="(max-width: 719px)" />
+            <source srcSet={model.src} media="(min-width: 500px)" />
+            <source srcSet={model.srcV} media="(max-width: 499px)" />
             <img
               id={model.name + "Img"}
               alt="Didn't work"
